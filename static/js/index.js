f711f613f4109de2ff47fa5e162980093ca5f6f9
Cassie-Yu <cassie-yu@hotmail.com>
2016-08-25 17:37:17 +0800

    侧边栏功能完备版

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,8 @@
 $(function(){
     
+/*******************************获取浏览器的宽度**********************************/  
+var width = $(window).width();
+$(".body_main").css("width",width-190+"px");  
 /*******************************菜单栏顶部点击展开收缩**********************************/  
 $(document).on("click",".left_menu_flex",function(){
     if($(".left_menu_flex .to_right").css("display")=="none"){
@@ -9,6 +12,13 @@ $(document).on("click",".left_menu_flex",function(){
         $(".left_main").css("width","50px");
         $(".left_menu").css("width","67px");
         $(".left_menu_right").css("left","50px");
+        if($(".left_menu_right").css("width") == "0px"){
+            $(".body_main").css("left","60px");
+            $(".body_main").css("width",width-60+"px");  
+        }else if($(".left_menu_right").css("width") == "180px"){
+            $(".body_main").css("left","240px");
+            $(".body_main").css("width",width-240+"px");
+        }
     }else if($(".left_menu_flex .to_right").css("display")=="block"){
         $(".left_menu_flex .to_right").css("display","none");
         $(".left_menu_flex .to_left").css("display","block");
@@ -16,6 +26,13 @@ $(document).on("click",".left_menu_flex",function(){
         $(".left_main").css("width","180px");
         $(".left_menu").css("width","197px");
         $(".left_menu_right").css("left","180px");
+        if($(".left_menu_right").css("width") == "0px"){
+            $(".body_main").css("left","190px");
+            $(".body_main").css("width",width-190+"px");
+        }else if($(".left_menu_right").css("width") == "180px"){
+            $(".body_main").css("left","370px");
+            $(".body_main").css("width",width-370+"px");
+        }
     }
 })
 
@@ -44,10 +61,11 @@ $(document).on("click",".collapse_option",function(){
     $(".menu_right_iconr").css("display","none");
     $(".left_menu_right").css("left","50px");
     $(".left_menu_right").width("0");
-    $(".left_menu_right").animate({width:"180px"});
+    $(".left_menu_right").css("width","180px");
     $(".collapse_option").removeClass("on");
     $(this).addClass("on");
-    
+    $(".body_main").css("left","240px"); 
+    $(".body_main").css("width",width-240+"px");
 }) 
 
 /***********************三级菜单栏图标点击菜单收缩**************************/ 
@@ -56,15 +74,32 @@ var height = $(window).height()/2 +　"px";
     
 $(document).on("click",".menu_right_iconl",function(){
     $(".menu_right_iconl").html("");
-    $(".left_menu_right").animate({width:"0"});
+    $(".left_menu_right").css("width","0");
     $(".menu_right_iconr").css({"display":"block","top":height});
     $(".menu_right_iconr").html("&#xe602;");
+    if($(".left_main").css("width") == "50px"){
+        $(".body_main").css("left","60px");
+        $(".body_main").css("width",width-60+"px");
+    }
+    else if($(".left_main").css("width") == "180px"){
+        $(".body_main").css("left","190px");
+        $(".body_main").css("width",width-190+"px");
+    }
+    
 })
 
 $(document).on("click",".menu_right_iconr",function(){
     $(".menu_right_iconl").html("&#xe603;");
-    $(".left_menu_right").animate({width:"180px"});
+    $(".left_menu_right").css("width","180px");
     $(this).css("display","none");
+    if($(".left_main").css("width") == "50px"){
+        $(".body_main").css("left","240px");
+        $(".body_main").css("width",width-240+"px");
+    }
+    else if($(".left_main").css("width") == "180px"){
+        $(".body_main").css("left","370px");
+        $(".body_main").css("width",width-370+"px");
+    }
 })
 
 /***********************二级菜单鼠标移入移出小提示**************************/ 
